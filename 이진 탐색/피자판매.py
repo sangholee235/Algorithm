a45ac717aa24edf148252532bb542f75682e8aa5
Choguesung <tkdgh1135@naver.com>
2023-01-06 09:19:00 +0900

    나무,예산 완료

--- 이진 탐색/피자판매.py
+++ 이진 탐색/피자판매.py
@@ -1,28 +1,34 @@
-from collections import deque
+n = int(input())
 
-needs = int(input())
+money = list(map(int,input().split()))
 
-m,n = map(int,input().split())
-a = []
-b = []
-na = []
-nb = []
-count = 0
+bg = int(input())
 
-for i in range(m):
-    a.append(int(input()))
+left = 0
+right = max(money)
 
-for i in range(n):
-    b.append(int(input()))
+#다 합해도 안넘는 예외처리
+if sum(money)<=bg:
+   print(max(money))
 
-for i in range(a):
-    for j in range(a):
-        na.append(a[i::i+j+1])
+else:
+    while left <= right:
+        mid = (left + right) // 2
+        sum = 0
 
-for i in range(b):
-    for j in range(b):
-        nb.append(b[i::i+j+1])
+        for i in money:
+            # 상한액을 초과하는 경우
+            if i > mid:
+                sum += mid
+            # 상한액보다 낮은 경우
+            else:
+                sum += i
 
+        if sum > bg:  # 예산초과하는경우 상한액을 낮춰야한다
+            right = mid - 1
+        else:
+            left = mid + 1
 
+    print(mid)
 
 
