e6e2a46e4653ba8a2c7a65dfa54ae62d9c8ac62a
Choguesung <tkdgh1135@naver.com>
2023-01-11 09:13:42 +0900

    브루트 포스

---
+++ 브루트 포스/안전 영역.py
@@ -0,0 +1,26 @@
+# 리스트 크기 입력 받기
+n = int(input())
+
+# 지역 리스트 2차원 초기화
+area = [[0]*2 for _ in range(n)]
+
+# 안전한 구역 count
+safe = 0
+
+# 현재 강수량
+rain = 0
+
+# 지역 정보 입력 받기
+for i in range(n):
+    area[i] = list(map(int,input().split()))
+
+while 1:
+    # 강수량 ++
+    for i in range(n):
+        for j in range(n):
+            area[i][j] += 1
+
+
+
+
+
