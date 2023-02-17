123eb1e2bc2b5f124ca9a6eeb3f29f78ef0245d8
LSH <121619411+Choguesung@users.noreply.github.com>
2023-02-18 05:40:24 +0900

    123

---
+++ 수학/달팽이는 올라가고 싶다.py
@@ -0,0 +1,13 @@
+up,down,tree = map(int, input().split())
+
+day = (tree-down)/(up-down)
+# tree - down 하는 이유는 정상에 올라가고 떨어지지 않기 위해서
+# 이렇게 안짜고 반복문 돌리면 시간 초과 걸림
+
+# 정수로 나눠 떨어지면
+if day == int(day):
+   print(int(day))
+
+# 그렇지 않다면 +1일을 해준다
+else:
+   print(int(day)+1)
