521e7efb446fa55cbee197bc578805c86b9ca395
LSH <121619411+Choguesung@users.noreply.github.com>
2023-02-23 14:54:21 +0900

    Update 달팽이는 올라가고 싶다.py

--- 수학/달팽이는 올라가고 싶다.py
+++ 수학/달팽이는 올라가고 싶다.py
@@ -2,6 +2,7 @@ up,down,tree = map(int, input().split())
 
 day = (tree-down)/(up-down)
 # tree - down 하는 이유는 정상에 올라가고 떨어지지 않기 위해서
+# up - down == 하루에 올라가는 양
 # 이렇게 안짜고 반복문 돌리면 시간 초과 걸림
 
 # 정수로 나눠 떨어지면
