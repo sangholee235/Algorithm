175e18bb6638cfd6202f2d82dc0dc3ebeff8e322
Choguesung <tkdgh1135@naver.com>
2023-02-27 19:55:39 +0900

    퇴사 커밋

--- 그리디/설탕배달.py
+++ 그리디/설탕배달.py
@@ -15,3 +15,18 @@ while N >= 0: # 설탕 소모 할때 까지 반복
 
 else:
     print(-1)
+
+count = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        count += N // 5
+        print(count)
+        break
+    N -= 3
+    count += 1
+
+else:
+    print(-1)
+
+
