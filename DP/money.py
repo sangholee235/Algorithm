bcfeb33d5916042419fb002ba4fdb723cfb86675
LSH <121619411+Choguesung@users.noreply.github.com>
2023-11-07 09:04:22 +0900

    Create money.py

---
+++ DP/money.py
@@ -0,0 +1,21 @@
+n,m = map(int, input().split())
+
+array = []
+
+for i in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m+1)
+
+d[0] = 0
+for i in range(n): # 화폐의 개수만큼 반복문을 돈다
+    for j in range(array[i], m+1): # 화폐의 가격부터 총량까지 반복문을 돈다
+        if d[j-array[i]] != 10001:
+                d[j] = min(d[j], d[j-array[i]] + 1)
+
+
+if d[m] == 10001:
+     print(-1)
+
+else:
+     print(d[m])
