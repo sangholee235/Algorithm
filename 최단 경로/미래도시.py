5d87b1eb88b0fb6331362d26c79762c846e74dfc
LSH <121619411+Choguesung@users.noreply.github.com>
2023-11-09 19:40:26 +0900

    dwd

--- 최단 경로/미래도시.py
+++ 최단 경로/미래도시.py
@@ -9,14 +9,15 @@ for a in range(1, n+1):
     for b in range(1, n+1):
         if a == b:
             graph[a][b] == 0
-            graph[b][a] == 0
 
 
 # 각 간선에 대한 정보를 입력받아 그값으로 초기화
 for _ in range(m): 
-    a,b,c = map(int,input().split())
-    graph[a][b] = c
+    a,b = map(int,input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
 
+x,k = map(int, input().split())
 
 # 플로이드 워셜 알고리즘 수행
 for k in range(1, n+1):
@@ -24,3 +25,5 @@ for k in range(1, n+1):
         for b in range(1, n+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
+
+distance = graph[1][k] + graph[k][x]
