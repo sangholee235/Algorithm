167df766f74552fed534beee2c4425fa1af38ca9
LSH <121619411+Choguesung@users.noreply.github.com>
2023-12-25 21:42:17 +0900

    solved: problems

--- 그리디/1로만들기.py
+++ 그리디/1로만들기.py
@@ -3,19 +3,17 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-count = 0
 
-while n != 1:
-    if n // 3 == 0:
-       n = n // 3
-       count += 1
-    
-    elif n // 2 == 0:
-         n = n // 2
-         count += 1
-    
-    else:
-        n = n-1
-        count += 1
+d = [0] * (n+1)
 
-print(count)
+for i in range(2,n+1):
+
+    d[i] = d[i-1] + 1
+
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2] + 1)
+    
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+    
+print(d[n])
