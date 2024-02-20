43550e8bcbc2b5f6c73846cd31fdb88ff35d05d3
LSH <121619411+Choguesung@users.noreply.github.com>
2024-02-21 00:00:29 +0900

    dw

--- DFS,BFS/영역구하기.py
+++ DFS,BFS/영역구하기.py
@@ -3,7 +3,7 @@ sys.setrecursionlimit(10**6)
 
 M, N, K = map(int, sys.stdin.readline().split())
 
-# 2차원 n*m 을 채워넣음
+# 2차원 n*m 을 채움
 field = [[0 for _ in range(N)] for _ in range(M)] 
 
 for _ in range(K):
