21e9d5e4d38f85dfd0b300bb0de618a2de33c7e4
LSH <121619411+Choguesung@users.noreply.github.com>
2024-03-12 23:44:56 +0900

    23

--- 정렬/퀵 정렬.py
+++ 정렬/퀵 정렬.py
@@ -3,7 +3,7 @@ array = [5,7,9,0,3,1,6,2,4,8]
 def quick_sort(array,start,end):
     if start >= end: # 원소가 1개 남으면 종료
         return
-    pivot = start # 첫번째 원소를 피벗으로 한다
+    pivot = start # 첫번째 원소를 피벗으로 함
     left = start + 1
     right = end
 
