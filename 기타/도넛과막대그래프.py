d186aefab20fa5002419096a9381b5a4494733c9
LSH <121619411+Choguesung@users.noreply.github.com>
2024-03-23 06:48:36 +0900

    solve : donut

--- 기타/도넛과막대그래프.py
+++ 기타/도넛과막대그래프.py
@@ -20,7 +20,8 @@ def solution(edges):
             answer[2] += 1
         elif exchangeCnt[0] >= 2 and exchangeCnt[1] >= 2:
             answer[3] += 1
-        
+
+    # 모든 경우에서 2,3 경우를 빼면 1의 경우    
     answer[1] = (exchangeCnts[answer[0]][0] - answer[2] - answer[3])
         
         
