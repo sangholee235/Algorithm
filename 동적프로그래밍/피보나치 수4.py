a0bde4e77cabecbf89ed9575d5101f041de3157e
LSH <121619411+Choguesung@users.noreply.github.com>
2024-06-11 17:40:43 +0900

    2

--- 동적프로그래밍/피보나치 수4.py
+++ 동적프로그래밍/피보나치 수4.py
@@ -1,9 +1,9 @@
 n = int(input())
 
-s = [0,1,1]
+s = [0, 1, 1]
 
-for i in range(3,n+1):
-    # 하나전 + 두개전 해서 계속 추가
-    s.append(s[i-1]+s[i-2])
+for i in range(3, n + 1):
+    # 하나전 + 두개전, 계속 추가
+    s.append(s[i - 1] + s[i - 2])
 
 print(s[n])
