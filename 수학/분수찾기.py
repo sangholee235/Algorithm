ad00a2a1c2f1565e953a6a86967021d77299ce39
LSH <121619411+Choguesung@users.noreply.github.com>
2024-07-12 23:39:58 +0900

    2

--- 수학/분수찾기.py
+++ 수학/분수찾기.py
@@ -1,10 +1,9 @@
-
 x = int(input())
 
 line = 1
 
-# 입력 받은 수가 몇번째 라인(line)에 있고 그 라인에서 몇번째(x)에 있는지11
-while x>line:
+# 입력 받은 수가 몇번째 라인(line)에 있고 그 라인에서 몇번째(x)에 있는지
+while x > line:
     x -= line
     line += 1
 
@@ -18,5 +17,4 @@ else:
     a = line - x + 1
     b = x
 
-print(a,'/',b,sep='')
-
+print(a, "/", b, sep="")
